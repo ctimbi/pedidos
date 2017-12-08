342bf337e5acb36f8ff97b6c6df49f2e13b57172
Juan Perez <Estudiante@lab0400>
2017-12-08 07:54:17 -0500

    Agregue la clase Persona en el paquete modelo

---
+++ Pedidos/src/modelo/Persona.java
@@ -0,0 +1,28 @@
+package modelo;
+
+public class Persona {
+
+	private String cedula;
+	private String nombre;
+	private String direccion;
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	
+}
